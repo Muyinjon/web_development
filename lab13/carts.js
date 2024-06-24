1dfe020eaaed6f70d0edbb6a6c7b88452dd5f7ea
Muyinjon "M" Turobov <muyinjon2@gmail.com>
2024-06-24 19:08:15 -0400

    lab 13 update

--- lab13/carts.js
+++ lab13/carts.js
@@ -3,16 +3,16 @@ const unitprice_display = document.querySelector('.unitprice_display');
 const quantityitem_display = document.querySelector('.quantityitem_display');
 const totalitemprice_display = document.querySelector('.totalitemprice_display');
 const itemname_display = document.querySelector('.itemname_display');
-const itemdiscription_display = document.querySelector('.itemdiscription_display');
+const itemdiscription_display = document.querySelector('.itemdescription_display');
 const imgitem_display = document.querySelector('.imgitem_display');
 //get values for quantity and unitprice
 const priceitem = sessionStorage.getItem('salesprice1')
 const quantityitem = sessionStorage.getItem('quantityitem1')
 
 const totalitemprice = priceitem * quantityitem;
-const itemname = document.querySelector('.itemname1')
-const itemdiscription = document.querySelector('.itemdiscription1')
-const imgitem = document.querySelector('.imgitem1')
+const itemname = sessionStorage.getItem('itemname1')
+let itemdiscription = sessionStorage.getItem('itemdiscription1')
+const imgitem = sessionStorage.getItem('imgitem1')
 //set the values
 unitprice_display.innerHTML = priceitem;
 quantityitem_display.innerHTML = quantityitem;
--- lab14/lab14.py
+++ lab14/lab14.py
@@ -1,1 +1,16 @@
-print("hello world")
+##Muyinjon Turobov
+##Monday June 2024
+##Intro to Python
+import math
+import random
+
+print("Hello World")
+
+
+
+print("Example 1------------------------ calculating the hypotenuse")
+height = int(input("Enter your height: "))
+width = float(input("Enter your weight: "))
+hyp=(height**2+width**2)**0.5
+
+print(f"the collected the height is {height} and the weight is {width} and the hypotenuse is {hyp}")
